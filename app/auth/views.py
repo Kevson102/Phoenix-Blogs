85bf8bf9d324eced80de5d23c627c6ed28cfac02
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-11-14 13:55:35 +0300

    FINALIZES ON THE LOGIN FUNCTIONALITY OF THE APPLICATION

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from . import auth
 from ..models import User
 from .forms import RegistrationForm, LoginForm
 from .. import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -12,6 +12,7 @@ def login():
     user = User.query.filter_by(email = login_form.email.data).first()
     if user is not None and user.verify_password(login_form.password.data):
       login_user(user, login_form.remember.data)
+      # return redirect(request.args.get('next') or url_for('main.index'))
       return redirect(request.args.get('next') or url_for('main.index'))
     flash('Invalid username or password')
   title = "Login"
@@ -21,7 +22,7 @@ def login():
 def register():
   registration_form = RegistrationForm()
   if registration_form.validate_on_submit():
-    user = User(email = registration_form.email.data, username = registration_form.username.data, pass_secure = registration_form.password.data)
+    user = User(email = registration_form.email.data, username = registration_form.username.data, password = registration_form.password.data)
     
     db.session.add(user)
     db.session.commit()
@@ -29,3 +30,9 @@ def register():
     return redirect(url_for('auth.login'))
   title = "Create New Account"
   return render_template('auth/register.html', registration_form = registration_form, title=title)
+
+@auth.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("main.index"))
